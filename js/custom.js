5894efa38ee16869144b440ac02c86c111604086
caroso1222 <ce.roso398@gmail.com>
2015-11-13 20:04:22 -0500

    comit

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+
+/*$(document).ready(function(){
+	var i = 0;
+	for(i= 1; i< 7;i++){
+		$(".brands-".concat(i)).mouseover(function(){
+			var j = 0;
+			for(j = 1; j < 7; j++){
+				if(j!=i){
+					$(".brands-".concat(j)).css('padding','5px');
+				}
+			}
+		});
+	}
+	var i = 0;
+	for(i= 1; i< 7;i++){
+		$(".brands-".concat(i)).mouseout(function(){
+			var j = 0;
+			for(j = 1; j < 7; j++){
+				if(j!=i){
+					$(".brands-".concat(j)).css('padding','0px');
+				}
+			}
+		});
+	}
+
+});
+
+ $zopim(function() {
+     $zopim.livechat.window.show();
+  });*/
