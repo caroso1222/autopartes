1bf642bb5cd0283523da3e63cebf26c3b36cb81a
Carlos Roso <ce.roso398@gmail.com>
2015-11-15 16:05:50 -0500

    Added new features

--- js/custom.js
+++ js/custom.js
@@ -28,3 +28,27 @@
  $zopim(function() {
      $zopim.livechat.window.show();
   });*/
+
+$(".chat").click(function(){
+	$zopim.livechat.window.show();
+});
+
+$("#buscar-btn").click(function(){
+$zopim.livechat.say("Hola, estoy interesado en buscar una referencia en su catálogo.");
+/*$zopim.livechat.window.hide();*/
+});
+
+$("#btn-industrial").click(function(){
+$zopim.livechat.say("Hola, quiero conocer más acerca de la línea industrial.");
+/*$zopim.livechat.window.hide();*/
+});
+
+$("#btn-automotriz").click(function(){
+$zopim.livechat.say("Hola, quiero conocer más acerca de la línea automotriz.");
+/*$zopim.livechat.window.hide();*/
+});
+
+$("#btn-agricola").click(function(){
+$zopim.livechat.say("Hola, quiero conocer más acerca de la línea agrícola.");
+/*$zopim.livechat.window.hide();*/
+});
